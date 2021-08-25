8298a390ff45472dc1c7a2735cacaca6258db002
conformist89 <olhalavoryk@gmail.com>
2021-08-25 17:06:14 +0200

    Reads info (filename and tree) from input TOML config and retrieves signal, background and deploy data

---
+++ dataConverter/DFconverter.py
@@ -0,0 +1,22 @@
+from hipe4ml.tree_handler import TreeHandler
+import tomli
+import sys
+
+
+def convertDF(input_file):
+    """
+    Opens input file in toml format, retrives signal, background and deploy data
+    like TreeHandler objects
+
+    Parameters
+    ------------------------------------------------
+    df: str
+        input toml file
+    """
+    with open(str(input_file), encoding="utf-8") as inp_file:
+        inp_dict = tomli.load(inp_file)
+    signal = TreeHandler(inp_dict["signal"]["path"], inp_dict["signal"]["tree"])
+    background = TreeHandler(inp_dict["background"]["path"], inp_dict["background"]["tree"])
+    deploy =  TreeHandler(inp_dict["background"]["path"], inp_dict["background"]["tree"])
+
+    return signal, background, deploy
