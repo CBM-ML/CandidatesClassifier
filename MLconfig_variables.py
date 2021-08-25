681747b5fc2452e1a9402d577fb285ffcbc33344
conformist89 <olhalavoryk@gmail.com>
2021-08-25 19:01:33 +0200

    Added some comments to 1D correlation finctions

--- MLconfig_variables.py
+++ MLconfig_variables.py
@@ -10,11 +10,22 @@ from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
 
 import gc
 import matplotlib as mpl
+from scipy.stats import sem
 
 
 def calculate_correlation(df, vars_to_corr, target_var) :
+    """
+    Calculates correlations with target variable variable and standart errors
+    Parameters
+    ------------------------------------------------
+    df: pandas.DataFrame
+        imput data
+    vars_to_corr: list of str
+         variables that correlate with target value
+    target_var: str
+          variable that correlates with another variables mentioned in list
+    """
 
-    from scipy.stats import sem
 
     mean = df[target_var].mean()
     sigma = df[target_var].std()
@@ -34,6 +45,27 @@ def calculate_correlation(df, vars_to_corr, target_var) :
 
 
 def plot1Dcorrelation(vars_to_draw,var_to_corr, corr_signal, corr_signal_errors, corr_bg, corr_bg_errors, output_path):
+    """
+    Plots correlations
+    Parameters
+    ------------------------------------------------
+    vars_to_draw: list of str
+        variables that correlate with target value
+    var_to_corr: str
+         variables that correlate with target value
+    corr_signal: list
+          signal covariance coefficient between variable and target variable
+    corr_signal_errors:
+          signal covariance standart error of the mean
+    corr_bg: list
+          background covariance coefficient between variable and target variable
+    corr_bg_errors:
+         background covariance standart error of the mean
+    output_path:
+          path that contains output plot
+
+    """
+
     fig, ax = plt.subplots(figsize=(20,10))
     plt.errorbar(vars_to_draw, corr_signal, yerr=corr_signal_errors, fmt='')
     plt.errorbar(vars_to_draw, corr_bg, yerr=corr_bg_errors, fmt='')
