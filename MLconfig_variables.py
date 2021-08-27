7b76920c693f85a465accdff3fcfe71f28f87521
conformist89 <olhalavoryk@gmail.com>
2021-08-27 11:58:31 +0200

    Plot TProfile bin centers of each bin vs mean of every bin

--- MLconfig_variables.py
+++ MLconfig_variables.py
@@ -2,15 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-from matplotlib.font_manager import FontProperties
-
-from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
-                               AutoMinorLocator)
-
-import gc
-import matplotlib as mpl
 from scipy.stats import sem
+from scipy.stats import binned_statistic as b_s
+
+
 
 
 def calculate_correlation(df, vars_to_corr, target_var) :
@@ -77,3 +72,56 @@ def plot1Dcorrelation(vars_to_draw,var_to_corr, corr_signal, corr_signal_errors,
     plt.ylabel('Correlation coefficient', fontsize = 25)
     fig.tight_layout()
     fig.savefig(output_path+'/all_vars_corr-'+ var_to_corr+'.png')
+
+
+
+def profile_plot_func(df,variable_xaxis,variable_yaxis, sign, pdf_key, peak, mass_left, mass_right):
+
+    if sign == 1:
+        keyword = 'signal'
+    if sign == 0:
+        keyword = 'background'
+
+    if variable_xaxis=='mass':
+        df = df[(df['mass'] < mass_right) & (df['mass'] > mass_left)]
+        unit = r'mass, $ \frac{GeV}{c^2}$'
+
+    if variable_xaxis=='pT':
+        df = df[df['pT'] < 3]
+        unit = r'pT, $ \frac{GeV}{c}$'
+
+    fig, axs = plt.subplots(figsize=(20, 15))
+
+    bin_means, bin_edges, binnumber = b_s(df[variable_xaxis],df[variable_yaxis], statistic='mean', bins=25)
+    bin_std, bin_edges, binnumber = b_s(df[variable_xaxis],df[variable_yaxis], statistic='std', bins=25)
+    bin_count, bin_edges, binnumber = b_s(df[variable_xaxis],df[variable_yaxis], statistic='count',bins= 25)
+    bin_width = (bin_edges[1] - bin_edges[0])
+    bin_centers = bin_edges[1:] - bin_width/2
+
+    nan_ind = np.where(np.isnan(bin_means))
+    bin_centers = np.delete(bin_centers, nan_ind)
+    bin_means = np.delete(bin_means, nan_ind)
+    bin_count = np.delete(bin_count, nan_ind)
+    bin_std = np.delete(bin_std , nan_ind)
+
+
+    plt.errorbar(x=bin_centers, y=bin_means, yerr=(bin_std/np.sqrt(bin_count)), linestyle='none', marker='.',mfc='red', ms=10)
+
+
+    plt.rcParams['font.size'] = '25'
+    for label in(axs.get_xticklabels() + axs.get_yticklabels()):
+        label.set_fontsize(25)
+
+
+    # plt.legend(fontsize=25)
+    plt.title('Mean of ' +variable_yaxis+ ' plotted versus bin centers of '+variable_xaxis+ \
+              '('+keyword+')', fontsize=25)
+    plt.xlabel(unit, fontsize=25)
+    plt.ylabel("Mean of each bin with the SEM ($\dfrac{bin\ std}{\sqrt{bin\ count}}$) of bin", fontsize=25)
+
+
+    plt.vlines(x=peak,ymin=bin_means.min(),ymax=bin_means.max(), color='r', linestyle='-')
+
+
+    fig.tight_layout()
+    plt.savefig(pdf_key,format='pdf')
