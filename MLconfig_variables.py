0422e9f1fbae2bb16e12163abed60eaeba4c2a39
conformist89 <olhalavoryk@gmail.com>
2021-08-27 15:02:44 +0200

    Implemented function plotting 2D distributions between 2 variables

--- MLconfig_variables.py
+++ MLconfig_variables.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from scipy.stats import sem
 from scipy.stats import binned_statistic as b_s
-
+import matplotlib as mpl
 
 
 
@@ -80,7 +80,7 @@ def profile_plot_func(df,variable_xaxis, x_unit, variable_yaxis, sign, pdf_key, 
     This function takes the entries of the variables and distributes them in 25 bins.
     The function then plots the bin centers of the first variable on the x-axis and
     the mean values of the bins of the second variable on the y-axis, along with its bin stds.
-    
+
     Parameters
     ------------------------------------------------
     df: pandas.DataFrame
@@ -152,6 +152,52 @@ def profile_plot_func(df,variable_xaxis, x_unit, variable_yaxis, sign, pdf_key, 
 
     plt.vlines(x=peak,ymin=bin_means.min(),ymax=bin_means.max(), color='r', linestyle='-')
 
+
+    fig.tight_layout()
+    plt.savefig(pdf_key,format='pdf')
+
+
+def plot2D(df, sample, sgn,x_axis_value, y_axis_value, range_x, range_y, pdf_key):
+    """
+
+    Plots 2-D histogram.
+
+    x_axis_value: e.g. 'mass'
+    y_axis_value: e.g. 'distance'
+    range_x: e.g. [1, 1.177]
+    range_y: e.g [0, 100]
+    pdf_key: e.g. output PDF file
+
+    """
+
+    fig, axs = plt.subplots(figsize=(15, 10))
+    cax = plt.hist2d(df[x_axis_value],df[y_axis_value],range=[range_x, range_y], bins=100,
+                norm=mpl.colors.LogNorm(), cmap=plt.cm.viridis)
+
+
+    if x_axis_value=='mass':
+        unit = r' $, \frac{GeV}{c^2}$ '
+        plt.vlines(x=1.115683,ymin=range_y[0],ymax=range_y[1], color='r', linestyle='-')
+
+    if x_axis_value=='pT':
+        unit = r' $, \frac{GeV}{c}$'
+
+
+    if sgn==1:
+        plt.title('Signal candidates ' + sample, fontsize = 25)
+
+    if sgn==0:
+        plt.title('Background candidates ' + sample, fontsize = 25)
+
+
+    plt.xlabel(x_axis_value+unit, fontsize=25)
+    plt.ylabel(y_axis_value, fontsize=25)
+
+
+
+    mpl.pyplot.colorbar()
+
+    plt.legend(shadow=True,title =str(len(df))+ " samples")
 
     fig.tight_layout()
     plt.savefig(pdf_key,format='pdf')
