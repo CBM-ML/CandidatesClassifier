be0852e1f77dcf237c40e3c0a7124863caf4e407
conformist89 <olhalavoryk@gmail.com>
2021-08-27 12:45:14 +0200

    Added comments to transform_df_to_log

--- log_helper.py
+++ log_helper.py
@@ -6,6 +6,20 @@ from read_configs import *
 
 
 def transform_df_to_log(df, vars, inp_file):
+    """
+    Transforms DataFrame to DataFrame with features in log scale
+    Parameters
+    ------------------------------------------------
+    df: pandas.DataFrame
+       input DataFrame
+
+    vars: list of str
+        vars to be transformed
+
+    inp_file:
+        config TOML file with list of features that should and shouldn't be
+        transformed to log scale 
+    """
     df_new = df.copy()
 
     non_log_x, log_x = read_log_vars(inp_file)
