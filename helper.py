5ceb0b27bb952077a0e9987bfc44b9763c9df9a4
conformist89 <olhalavoryk@gmail.com>
2021-08-29 17:01:26 +0200

    File with helping functions

--- helper.py
+++ helper.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 from read_configs import *
 
+import xgboost as xgb
+
 
 
 def transform_df_to_log(df, vars, inp_file):
@@ -12,13 +14,11 @@ def transform_df_to_log(df, vars, inp_file):
     ------------------------------------------------
     df: pandas.DataFrame
        input DataFrame
-
     vars: list of str
         vars to be transformed
-
     inp_file:
         config TOML file with list of features that should and shouldn't be
-        transformed to log scale 
+        transformed to log scale
     """
     df_new = df.copy()
 
@@ -30,3 +30,23 @@ def transform_df_to_log(df, vars, inp_file):
             df_new[var+'_log'] = np.log(df_new[var])
             df_new = df_new.drop([var], axis=1)
     return df_new
+
+
+def xgb_matr(x_train, y_train, x_test, y_test):
+    """
+    To make machine learning algorithms more efficient on unseen data we divide
+    our data into two sets. One set is for training the algorithm and the other
+    is for testing the algorithm. If we don't do this then the algorithm can
+    overfit and we will not capture the general trends in the data.
+
+    Parameters
+    ----------
+    df_scaled: dataframe
+          dataframe with mixed signal and background
+
+    """
+
+    dtrain = xgb.DMatrix(x_train, label = y_train)
+    dtest=xgb.DMatrix(x_test, label = y_test)
+
+    return dtrain, dtest
