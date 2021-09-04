a3a2a661541b83bc1084e5d921c36ecfc6b3287e
conformist89 <olhalavoryk@gmail.com>
2021-09-04 14:39:45 +0200

    Corrected deploy invoke

--- DFconverter.py
+++ DFconverter.py
@@ -17,6 +17,6 @@ def convertDF(input_file):
         inp_dict = tomli.load(inp_file)
     signal = TreeHandler(inp_dict["signal"]["path"], inp_dict["signal"]["tree"])
     background = TreeHandler(inp_dict["background"]["path"], inp_dict["background"]["tree"])
-    deploy =  TreeHandler(inp_dict["background"]["path"], inp_dict["background"]["tree"])
+    deploy =  TreeHandler(inp_dict["deploy"]["path"], inp_dict["deploy"]["tree"])
 
     return signal, background, deploy
